7751f322d39755f8a11b8ffc3b7f28c73eac6625
haikaljs <haikaljavascript@gmail.com>
2024-06-20 15:37:32 +0800

    Install vue-router and create routes

--- vue-admin/src/main.js
+++ vue-admin/src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
 import store from "./store";
+import router from "./router";
 import "./style.css";
 import App from "./App.vue";
 
-createApp(App).use(store).mount("#app");
+const app = createApp(App);
+app.use(router); // Use Vue Router
+app.use(store);
+app.mount("#app");
