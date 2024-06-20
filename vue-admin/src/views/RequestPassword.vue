348c781740d4ef966214ee399a436c3637364a35
haikaljs <haikaljavascript@gmail.com>
2024-06-21 07:58:08 +0800

    Create request password reset and reset password pages

--- vue-admin/src/views/RequestPassword.vue
+++ vue-admin/src/views/RequestPassword.vue
@@ -33,7 +33,7 @@
             <h2
                 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
             >
-                Sign in to your account
+                Request Reset Password
             </h2>
         </div>
 
@@ -55,32 +55,12 @@
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
                     </div>
-                </div>
 
-                <div>
-                    <div class="flex items-center justify-between">
-                        <label
-                            for="password"
-                            class="block text-sm font-medium leading-6 text-gray-900"
-                            >Password</label
-                        >
-                    
-                    </div>
-                    <div class="mt-2">
-                        <input
-                            id="password"
-                            name="password"
-                            type="password"
-                            autocomplete="current-password"
-                            required
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
                     <div class="text-sm">
                         <router-link
-                            :to="{ name: 'requestPassword' }"
+                            :to="{ name: 'login' }"
                             class="font-semibold text-indigo-600 hover:text-indigo-500"
-                            >Forgot your password?</router-link
+                            >Remember your password?</router-link
                         >
                     </div>
                 </div>
@@ -90,7 +70,7 @@
                         type="submit"
                         class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
-                        Sign in
+                        Submit
                     </button>
                 </div>
             </form>
