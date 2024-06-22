4be6b107a0f0927049d4789c0c7f73c94dccd057
haikaljs <haikaljavascript@gmail.com>
2024-06-22 17:53:39 +0800

    Create admin panel sidebar

--- vue-admin/src/components/AppLayout.vue
+++ vue-admin/src/components/AppLayout.vue
@@ -1,22 +1,22 @@
 <template>
-    <div
-        class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
-    >
-        <div class="mx-w-md w-full space-y-8">
-            <div>
-                <h2
-                    class="mt-6 text-center text-3xl font-entrabold text-gray-900"
-                >
-                    {{ title }}
-                </h2>
-            </div>
-
-            <router-view></router-view>
+    <div class="min-h-full flex">
+        <!-- Sidebar -->
+        <Sidebar />
+        <!-- Sidebar end -->
+        <div class="flex-1">
+            <header class="h-8 shadow bg-white">Header</header>
+            <!-- Content -->
+            <main>
+                <router-view></router-view>
+            </main>
+            <!-- Content -->
         </div>
     </div>
 </template>
 
 <script setup>
+import Sidebar from "./Sidebar.vue";
+
 const { title } = defineProps({
     title: String,
 });
---
+++ vue-admin/src/components/Sidebar.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="w-[200px] bg-indigo-700 text-white py-4 px-2">
+        <router-link
+            :to="{ name: 'app.dashboard' }"
+            class="flex items-center transition-colors hover:bg-black/30 p-2 rounded"
+        >
+            <span class="mr-2">
+                <HomeIcon class="w-5" />
+            </span>
+            <span class="text-sm">Dashboard</span>
+        </router-link>
+        <router-link
+            :to="{ name: 'app.dashboard' }"
+            class="flex items-center transition-colors hover:bg-black/30 p-2 rounded"
+        >
+            <span class="mr-2">
+                <TvIcon class="w-5" />
+            </span>
+            <span class="text-sm">Products</span>
+        </router-link>
+        <router-link
+            :to="{ name: 'app.dashboard' }"
+            class="flex items-center transition-colors hover:bg-black/30 p-2 rounded"
+        >
+            <span class="mr-2">
+                <UsersIcon class="w-5" />
+            </span>
+            <span class="text-sm">Users</span>
+        </router-link>
+        <router-link
+            :to="{ name: 'app.dashboard' }"
+            class="flex items-center transition-colors hover:bg-black/30 p-2 rounded"
+        >
+            <span class="mr-2">
+                <ChartBarIcon class="w-5" />
+            </span>
+            <span class="text-sm">Reports</span>
+        </router-link>
+    </div>
+</template>
+
+<script setup>
+import {
+    HomeIcon,
+    TvIcon,
+    UsersIcon,
+    ChartBarIcon,
+} from "@heroicons/vue/24/outline";
+</script>
+
+<style scoped></style>
